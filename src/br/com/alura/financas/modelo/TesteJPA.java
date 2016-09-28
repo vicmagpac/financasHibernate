fcf297acee217237a8ac605e6de23f0ad9038e78
Victor Magalhães Pacheco <victor.magalhaesp@gmail.com>
2016-09-27 21:50:24 -0300

    Estados ORM hibernate - Detached, Managed e Transient

--- src/br/com/alura/financas/modelo/TesteJPA.java
+++ src/br/com/alura/financas/modelo/TesteJPA.java
@@ -8,23 +8,23 @@ import br.com.alura.financas.util.JPAUtil;
 
 public class TesteJPA {
 	public static void main(String args[]) {
-				
-		/*
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas2");
-		EntityManager manager = entityManagerFactory.createEntityManager();
-		*/
-		EntityManager manager = new JPAUtil().getEntityManager();		
 		
+		/*
 		Conta conta = new Conta();
 		conta.setTitular("Victor Magalhães");
 		conta.setBanco("Bradesco");
 		conta.setAgencia("123456");
 		conta.setNumero("654");
+		*/
 		
+		EntityManager manager = new JPAUtil().getEntityManager();		
 		manager.getTransaction().begin();
-		manager.persist(conta);
-		manager.getTransaction().commit();
+	
 		
+		Conta conta = manager.find(Conta.class, 7);
+		conta.setTitular("Victor Mag");
+			
+		manager.getTransaction().commit();
 		manager.close();
 	}
 }
